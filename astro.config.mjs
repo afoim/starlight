bf1651e2e3571e81a914785aed9afb1609c008ff
afoim <acofork@qq.com>
2024-12-09 23:03:06 +0800

    初始化

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,41 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://onani.cn',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
+			title: 'AcoFork Docs',
+			favicon: '/AcoFork.png',
+			disable404Route: true,
+			locales: {
+				root: {
+				  label: '简体中文',
+				  lang: 'zh-CN',
+				},
+			  },
+			editLink: {
+				baseUrl: 'https://github.com/afoim/starlight/edit/main/docs/',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			logo: {
+				src: '/public/AcoFork.webp',
+				alt: 'AcoFork Docs',
+			},
+			social: {
+				github: 'https://github.com/afoim',
+			},
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{ label: 'Example Guide', slug: '开始/index' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
 		}),
 	],
 });
---
+++ src/content/docs/开始/index.md
@@ -0,0 +1,8 @@
+---
+title: 碎碎念
+description: 碎碎念
+---
+
+本人曾使用多个博客框架（虽然你现在看到的Starlight是一个文档框架），比如xLog、WordPress、Hexo、Halo、Fuwari、Typecho、VitePress、自己用Qwik手搓
+
+但我在某一天翻阅Astro文档的时候发现它的文档做的很好（甚至有ToDo），欸！就这样发现了名为Starlight的文档框架，在我看来，它比VitePress更好看，总之现在就用啦！
